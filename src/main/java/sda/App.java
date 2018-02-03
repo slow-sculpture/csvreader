22051c4d44fcfadcf4993f576ebf1aa1fa528ad2
ChristianR <christian.rogowski@yahoo.pl>
2018-02-03 14:57:38 +0100

    git init

---
+++ src/main/java/sda/App.java
@@ -0,0 +1,19 @@
+package sda.csvreader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        //TODO: 1.  utworzyc szkielet klast, ktoea otworzy plik
+
+        //TODO: 2. utworzyc klase/metode odpowiadajaca za przetworzenie pliku
+
+        //TODO: 3. utworzyc metode, ktora wyciagnie z pierwszego wiersza pliku naglowki
+
+        //TODO: 4. utworzyc merode, ktora przetworzy plik linia po linii oraz wrzuci rekordy z wiersza
+        //TODO:    do mapy uzywajac naglowkow z poprzedniego kroku jako kluczy dla mapy
+    }
+}
